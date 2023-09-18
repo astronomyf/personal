a000d95439443fe1b53ae6a656df6e1736fa5db4
Francesco Violante <47946866+astronomyf@users.noreply.github.com>
2023-09-18 18:34:06 +0100

    feat: added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,24 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import rehypePrettyCode from "rehype-pretty-code";
-
 import react from "@astrojs/react";
 
 // Syntax highlighting theme for Markdown code blocks
+import mdx from "@astrojs/mdx";
 const options = {
   theme: {
     dark: "github-dark-dimmed",
-    light: "github-light",
+    light: "github-light"
   },
-  keepBackground: false,
+  keepBackground: false
 };
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   markdown: {
     syntaxHighlight: false,
-    rehypePlugins: [[rehypePrettyCode, options]],
-  },
+    rehypePlugins: [[rehypePrettyCode, options]]
+  }
 });
